4dc44a6d831c37573407847254481f1eb9d3a889
Max Block <maxblock@pm.me>
2020-12-25 11:25:39 +0300

    update

---
+++ t0__test.py
@@ -0,0 +1,12 @@
+import os
+
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = telebot.TeleBot(TOKEN)
+
+bot.send_message(CHAT_ID, "test")
